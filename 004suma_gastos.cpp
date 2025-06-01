090d0d184c91ddb2359b8445ecd272f80e1ce2e7
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-05-31 23:55:38 -0500

    Añadiendo una validación para que los gastos ingresados no puedan ser negativos y un contador para saber cuántos gastos fueron ingresados

--- 004suma_gastos.cpp
+++ 004suma_gastos.cpp
@@ -7,16 +7,23 @@ int main() {
     float gasto;
     float suma = 0;
     char continuar;
+    int contador = 0;
 
     do {
         cout << "Ingrese un gasto: S/.";
         cin >> gasto;
-        suma += gasto;
+        if (gasto > 0) {
+            suma += gasto;
+            contador++;
+        } else {
+            cout << "El gasto debe ser mayor a 0." << endl;
+        }
         cout << "Desea ingresar otro gasto? (S/N): ";
         cin >> continuar;
     } while (continuar == 'S' || continuar == 's');
 
     cout << "Gasto total: S/." << suma << endl;
+    cout << "Cantidad de gastos ingresados: " << contador << endl;
 
     return 0;
 }
